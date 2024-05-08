6520773e3fd7a0baffd320c0aab6d1a38653c7df
Suquiinho <mathewsavila2013@gmail.com>
2024-05-08 05:39:56 -0300

    feat: add category item and list components

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -7,11 +7,11 @@ const CategoryList = async () => {
   // -pegar as categorias do banco de dados
   // renderizar um item pra cada categoria
   return (
-    <>
+    <div className="grid grid-cols-2 gap-3">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
-    </>
+    </div>
   );
 };
 
